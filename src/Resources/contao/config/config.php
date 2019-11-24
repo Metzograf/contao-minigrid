d034f11fb3d58f15e315d03fe362e87bb51debbd
Metzograf <metzograf@users.noreply.github.com>
2019-11-24 18:44:46 +0100

    fix

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -8,13 +8,4 @@ $GLOBALS['TL_CTE']['blocks'] = array(
 	'blockStop'	=> 'ContentBlockStop'
 );
 
-if(VERSION <= 4.5) {
-    if (TL_MODE === 'FE') { // Früher TL_MODE == 'FE'
-        // Pfad ggf. anpassen
-        // Alle Dateien in /src/Ressources/public werden unter /web/bundles/bundle-name
-        // als Symlink veröffentlicht nach composer install/update
-        $GLOBALS['TL_CSS'][] = 'bundles/contao-minigrid-bundle/mingrid.css';
-    }
- 
-}
-
+$GLOBALS['TL_CSS'][] = 'bundles/mingrid/minigrid.css';
