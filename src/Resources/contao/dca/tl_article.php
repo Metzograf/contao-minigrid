801297691c523484e6cb918095d9e7fb107fd8fe
Florian Metzner <metzner@metzograf.de>
2019-12-03 15:50:58 +0100

    mobile width

---
+++ src/Resources/contao/dca/tl_article.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * This file is part of the Minigrid Bundle.
+ *
+ * (c) Florian Metzner <https://github.com/metzograf>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+ 
+// Add fullwidth field
+$GLOBALS['TL_DCA']['tl_article']['palettes']['default'] = str_replace 
+( 
+    'cssID', 
+    'cssID,fullwidth', 
+    $GLOBALS['TL_DCA']['tl_article']['palettes']['default'] 
+); 
+
+// configurate field
+$GLOBALS['TL_DCA']['tl_article']['fields']['fullwidth'] = array 
+( 
+    'label'                   => array('Ganze Breite','Artikel über die gesamte Breite anzeigen (z.B. Slider & Banner).'),
+    'default'                 => 0,
+	'exclude'                 => true,
+	'inputType'               => 'checkbox',
+	'eval'                    => array('tl_class'=>'w50 clr'),
+	'sql'                     => "char(1) NOT NULL default ''"
+);
+
