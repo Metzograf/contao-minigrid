72f2935f507bbd2ea695182103bd09fb8609f38c
Metzograf <metzograf@users.noreply.github.com>
2020-04-20 14:45:12 +0200

    Add Support for DMA Elementgenerator

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -16,6 +16,9 @@ $GLOBALS['TL_DCA']['tl_content']['palettes']['blockStop'] = '{type_legend},type;
 
 $GLOBALS['TL_DCA']['tl_content']['palettes'] = str_replace('cssID;','cssID;{minigrid_legend},minirow,minigrid;',$GLOBALS['TL_DCA']['tl_content']['palettes']); 
 
+// DMA
+$GLOBALS['TL_DCA']['tl_content']['palettes'] = str_replace('cssID,space;','cssID,space;{minigrid_legend},minirow,minigrid;',$GLOBALS['TL_DCA']['tl_content']['palettes']); 
+
 /* Gallery */
 $GLOBALS['TL_DCA']['tl_content']['palettes'] = str_replace('useHomeDir;','useHomeDir;{minigrid_legend},minirow,minigrid;',$GLOBALS['TL_DCA']['tl_content']['palettes']); 
 
